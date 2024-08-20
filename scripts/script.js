2f2a3b36c6b1322c268ccc443ee534aeb7681334
umutaaslan <umutalperaslan705@hotmail.com>
2024-08-20 17:36:24 +0300

    added getComputerChoice and getHumanChoice functions

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,29 @@ const computerChoice = getComputerChoice();
 let humanScore;
 let computerScore;
 
+function getHumanChoice(){
+    let userInput = prompt("which is your choice? (Rock, Paper, Scissor)");
+    if (userInput != null && userInput != undefined && userInput != ""){
+        return userInput;
+    }
+    else {
+        alert("please choose one");
+        getHumanChoice();
+    }
+}
+
+function getComputerChoice(){
+    let randomNumber = Math.floor(Math.random() * 3);
+    switch(randomNumber){
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissor";    
+    }
+}
+
+
+
+
